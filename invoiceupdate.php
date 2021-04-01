c168044ebc30025180f8233674502614e23705e3
Aye_Lwin_Soe <ayelwinsoe.it2018@gmail.com>
2021-04-01 22:47:24 +0630

    change data

--- invoiceupdate.php
+++ invoiceupdate.php
@@ -6,11 +6,11 @@
 	$month = date('M');
 	$day = date('d');
 	$year = date('Y');
-
+$invoiceitemsql= "SELECT * from invoice_items   WHERE invoice_id=1";var_dump($invoiceitemsql);die();
 	$invoicedate = $today;
 
 	$product = $_POST['product'];
-	/*var_dump($product);
+	/*var_dump($product[0]);
 	die();*/
 	$qty = $_POST['qty'];
 	$price = $_POST['price'];
@@ -24,63 +24,38 @@
 	}else{
 		$tax = $tax1;
 	}
-	$invoicesql = "INSERT INTO invoices (invoicename, total, tax, invoicedate) VALUES(:invoicename, :total_amount, :tax, :invoicedate)";
-
+	$invoicesql = "UPDATE invoices SET invoicename=:invoicename, total=:total_amount, tax=:tax, invoicedate=:invoicedate WHERE id=:id";
+	$id = 1;
     $invoice_stmt= $pdo->prepare($invoicesql);
+    $invoice_stmt->bindParam(':id',$id);
 	$invoice_stmt->bindParam(':invoicename',$invoicename);
 	$invoice_stmt->bindParam(':total_amount',$total_amount);
 	$invoice_stmt->bindParam(':tax',$tax);
 	$invoice_stmt->bindParam(':invoicedate',$invoicedate);
 	$invoice_stmt->execute();
-	$invoiceid = $pdo->lastInsertId();
+	$invoiceid = 1;
 
 	for ($i=0; $i < count($product); $i++) { 
 		$productname = $product[$i];
 		$pricename = $price[$i];
 		$qtyname = $qty[$i];
 		
-		$invoiceitemsql= "INSERT INTO invoice_items (item_name, item_qty, item_price,invoice_id) VALUES (:productname, :qtyname, :pricename,:invoiceid)";
-
+		$invoiceitemsql= "UPDATE invoice_items SET item_name=:productname, item_qty=:qtyname, item_price=:pricename WHERE invoice_id=:id";
 		$invoiceitem_stmt= $pdo->prepare($invoiceitemsql);
+		$invoiceitem_stmt->bindParam(':id',$id);
 		$invoiceitem_stmt->bindParam(':productname',$productname);
 		$invoiceitem_stmt->bindParam(':qtyname',$qtyname);
 		$invoiceitem_stmt->bindParam(':pricename',$pricename);
-		$invoiceitem_stmt->bindParam(':invoiceid',$invoiceid);
+		//$invoiceitem_stmt->bindParam(':invoiceid',$invoiceid);
 		$invoiceitem_stmt->execute();
+
 	}
 
 	if($invoiceitem_stmt->rowCount()){
-			header("Location:tableadd.html");
+			header("Location:tableadd.php");
 		}
 		else{
 			echo " Error !";
 		}
 
 ?>
-
-<?php  
-	require 'db_connect.php';
-
-	$id=$_POST['id'];
-	$name=$_POST['name'];
-	$category=$_POST['category'];
-
-
-	$sql="UPDATE subcategories SET name=:name, category_id=:category  WHERE id=:id ";
-	
-	$stmt=$pdo->prepare($sql);
-	$stmt->bindParam(':id',$id);
-	$stmt->bindParam(':name',$name);
-	$stmt->bindParam(':category',$category);
-
-	$stmt->execute();
-
-	if($stmt->rowCount())
-	{
-		header("location:subcategory_list");
-	}
-	else
-	{
-		echo "Error!";
-	}
- ?>
